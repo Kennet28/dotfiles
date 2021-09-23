15395e8ec09ac681ba3d6eb57c8eb519567aa892
Roger Castillo Chan <59662768+Kennet28@users.noreply.github.com>
2021-09-23 07:09:51 -0500

    dont worry

--- qtile/config.py
+++ qtile/config.py
@@ -1,27 +1,134 @@
-from os import system
+from typing import List  # noqa: F401
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-from settings.theme import colors
-from settings.utils import base, separator, icon, powerline
-from settings.keys import keys,mod
-from settings.screens import screens
-from settings.widgets import widget_defaults,extension_defaults
-from settings.workspaces import groups
+import os
+# from libqtile import hook
 
+# from settings.keys import mod, keys
+# from settings.groups import groups
+# from settings.layouts import layouts, floating_layout
+# from settings.widgets import widget_defaults
+# #, extension_defaults
+# from settings.screens import screens
+# from settings.mouse import mouse
+# from settings.path import qtile_path
 
+from os import path,system
+mod = "mod4"
 terminal = guess_terminal()
 
-#layouts
+keys = [
+    # Switch between windows in current stack pane
+    Key([mod], "k", lazy.layout.down(),
+        desc="Move focus down in stack pane"),
+    Key([mod], "j", lazy.layout.up(),
+        desc="Move focus up in stack pane"),
+
+    # Move windows up or down in current stack
+    Key([mod, "control"], "k", lazy.layout.shuffle_down(),
+        desc="Move window down in current stack "),
+    Key([mod, "control"], "j", lazy.layout.shuffle_up(),
+        desc="Move window up in current stack "),
+
+    # Switch window focus to other pane(s) of stack
+    Key([mod], "space", lazy.layout.next(),
+        desc="Switch window focus to other pane(s) of stack"),
+
+    # Swap panes of split stack
+    Key([mod, "shift"], "space", lazy.layout.rotate(),
+        desc="Swap panes of split stack"),
+
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"),
+    Key([mod], "Return", lazy.spawn("alacritty"), desc="Launch terminal"),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
+    Key([mod], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
+    # rofi
+    Key([mod], "m", lazy.spawn("rofi -show run")),
+    Key([mod, 'shift'], "m", lazy.spawn("rofi -show")),
+    # Volumen
+    Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 5")),
+    Key([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
+    # Brillo
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
+]
+
+groups = [Group(i) for i in "asdfuiop"]
+
+for i in groups:
+    keys.extend([
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
+
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+    ])
 layouts = [
     layout.MonadTall()
 ]
 
+widget_defaults = dict(
+    font='sans',
+    fontsize=14,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
 
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+                widget.QuickExit(),
+            ],
+            24,
+        ),
+    ),
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+                widget.QuickExit(),
+            ],
+            24,
+        ),
+    ),
+]
 
 # Drag floating layouts.
-#TODO: I NEED THINK ABOUT THIS
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
@@ -55,12 +162,12 @@ floating_layout = layout.Floating(float_rules=[
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 
-# Name
+
 wmname = "LG3D"
-# Autostart config
+
 autoStart = [
     "setxkbmap -layout latam",
-    "feh --bg-fill ~/.config/qtile/wallpapers/01.png"
+    "feh --bg-fill /home/kennet28/.config/qtile/wallpapers/01.png"
 ]
 
 for command in autoStart:
---
+++ qtile/thisfunc.py
@@ -0,0 +1,66 @@
+from os import system
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+from settings.theme import colors
+from settings.utils import base, separator, icon, powerline
+from settings.keys import keys,mod
+from settings.screens import screens
+from settings.widgets import widget_defaults,extension_defaults
+from settings.workspaces import groups
+
+
+terminal = guess_terminal()
+
+#layouts
+layouts = [
+    layout.MonadTall()
+]
+
+
+
+# Drag floating layouts.
+#TODO: I NEED THINK ABOUT THIS
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+# Name
+wmname = "LG3D"
+# Autostart config
+autoStart = [
+    "setxkbmap -layout latam"
+]
+
+for command in autoStart:
+    system(command)
